7813b6e663addd76763cba7ca528785ac47e0170
RANJITHA1706 <hranjitha661@gmail.com>
2025-06-29 20:01:13 +0530

    Delete assignment_week1 directory

--- assignment_week1/assignment_27thjune/recursion_sums/factorial.py
+++
@@ -1,7 +0,0 @@
-def factorial(number):
-    if number == 0 or number == 1:
-        return 1
-    else:
-        return number *factorial(number-1)
-num =int(input("Enter the number you want to find factorial : "))
-print(f"Factorial of {num} is ",factorial(num))    
